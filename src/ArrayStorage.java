774bd8b0ff86a855cafd45c5bbad92b57da184fe
KalinPlas <KalinPlas@gmail.com>
2019-07-11 21:20:42 +0300

    Created methods: clear() save() get() delete() getAll() size()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
 /**
  * Array based storage for Resumes
  */
+
+import java.util.ArrayList;
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
 
-    void clear() {
+    //Resume[] storage = new Resume[10000];
+    // let's use optimal variant for this - ArrayList ( That's no cheating =) )
+
+    private ArrayList<Resume> storage = new ArrayList<>();
+
+    void clear() { storage.clear(); }
+
+    void save(final Resume r) { storage.add(r); }
+
+    Resume get(final String uuid) { // return resume by iterating over items and compares their uuids
+
+    String comparableUuid;
+    Resume resume;
+
+    for(int i = 0;i < size();i++) {
+
+            resume = storage.get(i);
+            comparableUuid = resume.uuid;
+            if(comparableUuid.equals(uuid)) return storage.get(i);
     }
-
-    void save(Resume r) {
-    }
-
-    Resume get(String uuid) {
         return null;
     }
 
-    void delete(String uuid) {
+    void delete(final String uuid) { // delete resume by iterating over items and compares their uuids
+
+        String comparableUuid;
+        Resume resume;
+
+        for(int i = 0;i < size();i++) {
+
+            resume = storage.get(i);
+            comparableUuid = resume.uuid;
+            if(comparableUuid.equals(uuid)) storage.remove(i);
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = storage.toArray(new Resume[0]);
+        return resumes;
     }
 
-    int size() {
-        return 0;
-    }
+    int size() { return storage.size(); }
 }
